30bcb41da8ad075aafe491aae8534f25a933bf91
Luiz Felipe Rodrigues da silva <felipe32br@gmail.com>
2026-03-09 12:14:51 -0300

    upload do arquivo oraculo

--- oraculo.py
+++ oraculo.py
@@ -1,40 +1,33 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+import streamlit as st
 
+st.set_page_config(
+    layout="wide",
+    page_title="Oráculo",
+    page_icon="🔮"
+)
 
-df = pd.read_csv('dados.csv',sep = ';')  
+st.title("🔮 Oráculo")
 
+df = pd.read_csv('dados.csv', sep=';')
 
 vectorizer = TfidfVectorizer()
-
-tfidf_matrix = vectorizer.fit_transform(df['Perguntas']) 
-
+tfidf_matrix = vectorizer.fit_transform(df['Perguntas'])
 
 def responder_pergunta(pergunta_usuario):
-    
     pergunta_vetor = vectorizer.transform([pergunta_usuario])
-    
-    
     similaridades = cosine_similarity(pergunta_vetor, tfidf_matrix)
-    
-   
     indice_mais_similar = similaridades.argmax()
-    
-   
     return df.iloc[indice_mais_similar][' Respostas']
 
+pergunta = st.text_input("Digite sua pergunta:", placeholder="Ex: Horário de Funcionamento")
 
-print("Assistente Virtual - Digite 'sair' para encerrar")
-while True:
-    pergunta = input("\nComo posso te ajudar? ")
-    
-    if pergunta.lower() == 'sair':
-        print("Até logo!")
-        break
-        
+if pergunta:
     resposta = responder_pergunta(pergunta)
-    print(f"\nResposta: {resposta}")
+    st.write(resposta)
+
 
 
 
